2ebb2db8c1052aa38641490c1f96b1bd52a0d27e
Dmitry <dimkut@mail.ru>
2021-04-22 19:55:03 +0400

    yoga1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 window.addEventListener('DOMContentLoaded', function () {
   'use strict';
   let tab = document.querySelectorAll('.info-header-tab'),
-      info = document.querySelector('.info-header'),
-      tabContent = document.querySelectorAll('.info-tabcontent');
+    info = document.querySelector('.info-header'),
+    tabContent = document.querySelectorAll('.info-tabcontent');
 
-  function hideTabContent (a) {
+  function hideTabContent(a) {
     for (let i = a; i < tabContent.length; i++) {
       tabContent[i].classList.remove('show');
       tabContent[i].classList.add('hide');
@@ -13,18 +13,18 @@ window.addEventListener('DOMContentLoaded', function () {
 
   hideTabContent(1);
 
-  function showTabContent (el) {
+  function showTabContent(el) {
     hideTabContent(0);
     tabContent[el].classList.add('show');
     tabContent[el].classList.remove('hide');
   }
 
-  info.addEventListener ('click', function (event) {
-    for(let i = 0; i< tab.length; i++) {
+  info.addEventListener('click', function (event) {
+    for (let i = 0; i < tab.length; i++) {
       if (event.target == tab[i]) {
         showTabContent(i);
         break;
       }
-    } 
+    }
   });
 });
