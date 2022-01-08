fe6ee17f6287479381795fdd5bce61a431625671
Dmitry <dimkut@mail.ru>
2022-01-09 01:45:05 +0400

    calculator

--- js/script.js
+++ js/script.js
@@ -243,4 +243,46 @@ window.addEventListener('DOMContentLoaded', function () {
       }
     }
   });
+
+  // setInterval(function() {
+  //   plusSlides(1);
+  // }, 2000);
+
+  // calc
+  let persons = document.querySelectorAll('.counter-block-input')[0],
+    restDays = document.querySelectorAll('.counter-block-input')[1],
+    place = document.getElementById('select'),
+    totalValue = document.getElementById('total'),
+    personsSum = 0,
+    daysSum = 0,
+    total = 0;
+
+    totalValue.textContent = 0;
+
+    persons.addEventListener('input', function(){
+      personsSum = +persons.value;
+      total = (daysSum + personsSum) * 4000;
+      if (restDays.value == '' || persons.value == '') {
+        totalValue.textContent = 0;
+      } else {
+        totalValue.textContent = total;
+      }
+    });
+    restDays.addEventListener('input', function(){
+      daysSum = +restDays.value;
+      total = (daysSum + personsSum) * 4000;
+      if (restDays.value == '' || persons.value == '') {
+        totalValue.textContent = 0;
+      } else {
+        totalValue.textContent = total;
+      }
+    });
+    place.addEventListener('change', function(){
+      if (restDays.value == '' || persons.value == '') {
+        totalValue.textContent = 0;  
+      } else {
+        let a = total;
+        totalValue.textContent = a*this.options[this.selectedIndex].value;
+      }
+    });
 });
